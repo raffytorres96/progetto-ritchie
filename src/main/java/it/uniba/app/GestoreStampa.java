107eab8b52321e2d14619e1727d493bcaf4495c5
p-allegrezza <allegrezzapasquale@gmail.com>
2024-05-10 14:31:46 +0200

    aggiunti attributi di classe

--- src/main/java/it/uniba/app/GestoreStampa.java
+++ src/main/java/it/uniba/app/GestoreStampa.java
@@ -15,5 +15,41 @@ package it.uniba.app;
  */
 
 public class GestoreStampa {
+
+     /**
+   * Attributo statico che contiene il colore di default.
+   */
+  public static final String ANSI_RESET = "\u001B[0m";
+
+  /**
+   * Attributo statico che contiene il colore rosso.
+   */
+  public static final String ANSI_RED = "\u001B[31m";
+
+  /**
+   * Attributo statico che contiene il colore verde.
+   */
+  public static final String ANSI_GREEN = "\u001B[32m";
+
+  /**
+   * Attributo statico che contiene il colore di giallo.
+   */
+  public static final String ANSI_YELLOW = "\u001B[33m";
+
+  /**
+   * Attributo statico che contiene il colore di blu.
+   */
+  public static final String ANSI_BLUE = "\u001B[34m";
+
+  /**
+   * Attributo statico che contiene il colore di blu.
+   */
+  public static final String ANSI_ORANGE = "\u001B[38;2;255;165;0m";
+
+  /**
+   * Attributo statico che contiene il colore di blu.
+   */
+  public static final String ANSI_PINK = "\u001B[38;2;255;105;180m";
+
     
 }
