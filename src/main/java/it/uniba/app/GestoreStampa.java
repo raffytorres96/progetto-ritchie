1f1b965e58920b65aa417f414193ec521ca2c6a6
p-allegrezza <allegrezzapasquale@gmail.com>
2024-05-10 14:28:26 +0200

    ho creato la classe GestoreStampa ok relativa ECB e Responsabilità

---
+++ src/main/java/it/uniba/app/GestoreStampa.java
@@ -0,0 +1,19 @@
+package it.uniba.app;
+/**
+ * <<Boundary>>
+ * Responsabilità: Gestisce la stampa a video di messaggi.
+ */
+
+
+/**
+ * Classe che si occupa gestire la stampa a video dei messaggi
+ * del gioco. Per migliorare l'interfaccia e la UX dell'utente
+ * sono stati inseriti i colori e una particolare formattazione
+ * dei messaggi.
+ *
+ * @author GrruppoRitchie
+ */
+
+public class GestoreStampa {
+    
+}
