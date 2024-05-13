c52562d03087f5ed9a2b8e7731382a065b04bf22
p-allegrezza <allegrezzapasquale@gmail.com>
2024-05-13 11:37:44 +0200

    Aggiunta la classe Regola.java

---
+++ src/main/java/it/uniba/app/Regola.java
@@ -0,0 +1,16 @@
+/**
+ *  <<Entity>>
+ *  Responsabilità: Modella l'entità regola
+ *  che definisce da quali regole sarà governata la partità.
+ */
+
+package it.uniba.app;
+
+/**
+ * Classe che modella la regola.
+ *
+ * @author GruppoRitchie
+ */
+public class Regola {
+    
+}
