af41480c3f10409f6ad7d301bc38c54473635f97
p-allegrezza <allegrezzapasquale@gmail.com>
2024-05-13 11:40:06 +0200

    Aggiunti attributi alla classe Regola.java

--- src/main/java/it/uniba/app/Regola.java
+++ src/main/java/it/uniba/app/Regola.java
@@ -13,4 +13,22 @@ package it.uniba.app;
  */
 public class Regola {
     
+    /** Attributo di classe per identificare le regole classiche del gioco  */
+	private final static String CLASSICA="Classica";
+	
+	/** Attributo di classe per identificare le regole del gioco secodo la vesione Thomas  */
+	private final static String THOMAS="Thomas";
+	
+	/** Attributo di classe per identificare le regole del gioco secodo la vesione Assimilation  */
+	private final static String ASSIMILATION="Assimilation";
+	
+	/** Attributo di classe per identificare le regole del gioco secodo la vesione Variante a perdere  */
+	private final static String VARIANTE_PERDERE="Variante a Perdere";
+	
+	/** Attributo d'istanza che contine il tipo di regola selezionata  */
+	private String tipoRegola;
+	
+	/** Attributo d'istanza di controllo per verificare che la regola selezionata è corretta  */
+	private boolean regolaEsatta=false;
+    
 }
