ab1e16d5a45aa52b132649f27689de5cc2011945
p-allegrezza <allegrezzapasquale@gmail.com>
2024-05-14 16:58:13 +0200

    Aggiunto metodi getter e setter per la Classe Giocatore.java

--- src/main/java/it/uniba/app/Giocatore.java
+++ src/main/java/it/uniba/app/Giocatore.java
@@ -15,5 +15,44 @@ private String nome;
  /** Attributo d'istanza contenente il nome del Giocatore.  */
 private int statoGiocatore;
 
+/**
+* Costruttore di Giocatore.
+*/
+Giocatore(final String nomeInit, final int statoGiocatoreInit) {
+    nome = nomeInit;
+    statoGiocatore = statoGiocatoreInit;
+ }
+/**
+* Metodo get per lo stato del giocatore.
+
+* @return Variabile contenente lo stato del giocatore.
+*/
+public int getStatoGiocatore() {
+    return statoGiocatore;
+ }
+/**
+    * Metodo get per lo stato del giocatore.
+
+    * @param statoGiocatoreInit variabile contenente lo stato del giocatore.
+    */
+    public void setStatoGiocatore(final int statoGiocatoreInit) {
+        statoGiocatore = statoGiocatoreInit;
+    }
+/**
+    * Metodo get per il nome dell' giocatore.
+
+    * @return Variabile contenente il nome del giocatore.
+    */
+    public String getNome() {
+        return nome;
+    }
+/**
+    * Metodo set per il nome del giocatore.
+
+    *@param nomeInit variabile contenente il nome del giocatore.
+    */
+    public void setNome(final String nomeInit) {
+        nome = nomeInit;
+    }
 
 }
