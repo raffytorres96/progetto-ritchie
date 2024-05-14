17bd75b0f8099fac6dcdfc8e7aaa6d5ad67ce4a1
p-allegrezza <allegrezzapasquale@gmail.com>
2024-05-14 22:13:01 +0200

    Aggiunti attributi di istanza alla classe Partita

--- src/main/java/it/uniba/app/Partita.java
+++ src/main/java/it/uniba/app/Partita.java
@@ -23,6 +23,21 @@ private static final int GIOCATORE2 = 2;
 private static final int POSIZIONE0 = 0;
 
 /** attributo statico per identificare la cordinata da dare a setTavoliere.  */
-private static final int POSIZIOENE7 = 7;
+private static final int POSIZIONE7 = 7;
+
+/** attributo che indica se il gioco è finito o meno.  */
+private boolean giocoFinito = false;
+
+/** attributo che rappresenta la regola del gioco.  */
+private Regola regola;
+
+/** attributo che rappresenta il tavoliere del gioco.  */
+private Tavoliere tavoliere;
+
+/** attributo che rappresenta il giocatore1.  */
+private Giocatore giocatore1;
+
+/** attributo che rappresenta il giocatore2.  */
+private Giocatore giocatore2;
 
 }
