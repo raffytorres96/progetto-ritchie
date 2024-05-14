98904e2bbdcb96b14d21d57959cda555c34b8e21
p-allegrezza <allegrezzapasquale@gmail.com>
2024-05-14 22:41:53 +0200

    Aggiunti metodi alla classe Partita

--- src/main/java/it/uniba/app/Partita.java
+++ src/main/java/it/uniba/app/Partita.java
@@ -39,5 +39,73 @@ private Giocatore giocatore1;
 
 /** attributo che rappresenta il giocatore2.  */
 private Giocatore giocatore2;
+/**
+ * Costruttore della classe Partita.
+ * @param regolaGioco
+ * @param nome1
+ * @param nome2
+ */
+public Partita(final String regolaGioco, final String nome1, final String nome2) {
+
+this.tavoliere = new Tavoliere();
+this.tavoliere.setTavoliere(POSIZIONE0, POSIZIONE0, GIOCATORE1);
+this.tavoliere.setTavoliere(POSIZIONE7, POSIZIONE7, GIOCATORE1);
+this.tavoliere.setTavoliere(POSIZIONE0, POSIZIONE7, GIOCATORE2);
+this.tavoliere.setTavoliere(POSIZIONE7, POSIZIONE0, GIOCATORE2);
+this.regola = new Regola(regolaGioco);
+this.giocatore1 = new Giocatore(nome1, GIOCATORE1);
+this.giocatore2 = new Giocatore(nome2, GIOCATORE2);
+}
+    /**
+    * Metodo che restituisce il giocatore1.
+    */
+    public Giocatore getGiocatore1() {
+        return this.giocatore1;
+    }
+    /**
+    * Metodo che restituisce il giocatore2.
+    */
+    public Giocatore getGiocatore2() {
+        return this.giocatore2;
+    }
+   /**
+    * Metodo che restituisce il tavoliere.
+    */
+    public Tavoliere getTavoliere() {
+        return this.tavoliere;
+    }
+    /**
+    * Metodo che setta il tavoliere.
+    * @param riga
+    * @param colonna
+    * @param stato
+    */
+    public void setTavoliere(final int riga, final int colonna, final int stato) {
+        this.tavoliere.setTavoliere(riga, colonna, stato);
+    }
+    /**
+    * Metodo che restituisce la regola.
+    * @return
+    */
+    public Regola getRegola() {
+        return this.regola;
+    }
+    /**
+    * Metodo che restituisce se il gioco è finito o meno.
+    * @return
+     */
+    public boolean isGiocoFinito() {
+        return this.giocoFinito;
+
+    }
+    /**
+    * Metodo che setta se il gioco è finito o meno.
+    * @param isGiocoFinito
+    */
+    public void setGiocoFinito(final boolean isGiocoFinito) {
+        this.giocoFinito = giocoFinito;
+    }
 
 }
+
+
