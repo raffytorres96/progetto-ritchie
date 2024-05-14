9a895f2b02f3eba0b391f1502dc6bf07028316a0
p-allegrezza <allegrezzapasquale@gmail.com>
2024-05-14 16:42:34 +0200

    Creato file Giocatore.java

---
+++ src/main/java/it/uniba/app/Giocatore.java
@@ -0,0 +1,19 @@
+/**
+ *  <<Entity>>
+ *  Responsabilità: Modella l'entità Giocatore
+ *  del gioco.
+ */
+package it.uniba.app;
+/**
+ *  <<Entity>>
+ *  Responsabilità: Modella l'entità Giocatore
+ *  del gioco.
+ */
+public class Giocatore {
+ /** Attributo d'istanza contenente il nome del Giocatore.  */
+private String nome;
+ /** Attributo d'istanza contenente il nome del Giocatore.  */
+private int statoGiocatore;
+
+
+}
