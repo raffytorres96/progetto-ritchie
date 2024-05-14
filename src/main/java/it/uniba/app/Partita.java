2cda94a2c563688b261c57923f6a5d3346285541
p-allegrezza <allegrezzapasquale@gmail.com>
2024-05-14 22:09:44 +0200

    Aggiunti statici alla classe Partita

--- src/main/java/it/uniba/app/Partita.java
+++ src/main/java/it/uniba/app/Partita.java
@@ -11,4 +11,18 @@ package it.uniba.app;
  */
 public class Partita {
 
+    /** attributo statico per identificare lo stato della cella occupata
+ * nel tavoliere dalla pedina che corrisponde al giocatore1.  */
+private static final int GIOCATORE1 = 1;
+
+/** attributo statico per identificare lo stato della cella occupata
+ * nel tavoliere dalla pedina che corrisponde al giocatore2.  */
+private static final int GIOCATORE2 = 2;
+
+/** attributo statico per identificare la cordinata da dare a setTavoliere.  */
+private static final int POSIZIONE0 = 0;
+
+/** attributo statico per identificare la cordinata da dare a setTavoliere.  */
+private static final int POSIZIOENE7 = 7;
+
 }
