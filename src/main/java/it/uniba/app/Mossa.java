f451552464650f03d4a01e41629ad4324343544c
p-allegrezza <allegrezzapasquale@gmail.com>
2024-05-15 09:09:53 +0200

    Aggiunti attributi di istanza per la classe Mossa

--- src/main/java/it/uniba/app/Mossa.java
+++ src/main/java/it/uniba/app/Mossa.java
@@ -9,6 +9,34 @@ package it.uniba.app;
  * Classe che modella la mossa.
  */
 public class Mossa {
+    /**
+     * Attributo di istanza che rappresenta la coordinata di riga
+     * che identifica la cella su cui applicare
+     * la mossa.
+     */
+    private int riga;
 
+    /**
+     * Attributo di istanza che rappresenta  la coordinata di colonna
+     * che identifica la cella su cui applicare la mossa.
+     *
+     */
+    private int colonna;
+
+    /**
+     * Attributo di istanza che rappresenta il tavoliere
+     * su cui applicare la mossa.
+     */
+    private Tavoliere tavoliere;
+
+    /**
+     * Attributo di istanza che rappresenta il giocatore corrente.
+     */
+    private int giocatoreCorrente;
+
+    /**
+     * Attributo statico che rappresenta la dimensione del tavoliere.
+     */
+    public static final  int SIZE = 7;
 
 }
