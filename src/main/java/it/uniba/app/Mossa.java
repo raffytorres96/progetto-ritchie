24cdb1e4172ba02874dccd8701cae7576bc91055
p-allegrezza <allegrezzapasquale@gmail.com>
2024-05-15 08:57:18 +0200

    Creata la classe Mossa

---
+++ src/main/java/it/uniba/app/Mossa.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * <<Control>>
+ * Responsabilità: gestisce e modella
+ * il le mosse lecite effettuata dal giocatore.
+ */
+package it.uniba.app;
+/**
+ * Classe che modella la mossa.
+ */
+public class Mossa {
+
+
+}
